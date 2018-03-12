b66ed815a4ca02c878df6c701ede461ad2c69899
Loïc <ldivad@isep.fr>
2018-03-12 22:54:34 +0100

    add all the hosts

--- terraform.tf
+++ terraform.tf
@@ -4,20 +4,82 @@ provider "google" {
   region      = "europe-west1"
 }
 
-variable "ansible-install-cmd" {
-  type = "list",
-  default = [
-      "sudo apt-get update -y",
-      "sudo apt-get install software-properties-common -y",
-      "sudo apt-add-repository ppa:ansible/ansible -y",
-      "sudo apt-get update -y",
-      "sudo apt-get install ansible -y"
-    ]
-}
-
 resource "google_compute_instance" "controler" {
 
   name = "ctrl"
+  zone = "europe-west1-d"
+  machine_type = "n1-standard-2"
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-1710"
+    }
+  }
+  network_interface {
+    network = "default"
+    access_config { }
+  }
+
+
+  metadata_startup_script = "${file("./scripts/apt-get-ansible.sh")}"
+}
+
+resource "google_compute_instance" "loadbalancer" {
+
+  name = "ldbc"
+  zone = "europe-west1-d"
+  machine_type = "n1-standard-2"
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-1710"
+    }
+  }
+  network_interface {
+    network = "default"
+    access_config { }
+  }
+
+  metadata_startup_script = "${file("./scripts/apt-get-ansible.sh")}"
+}
+
+resource "google_compute_instance" "webapp01" {
+
+  name = "app01"
+  zone = "europe-west1-d"
+  machine_type = "n1-standard-2"
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-1710"
+    }
+  }
+  network_interface {
+    network = "default"
+    access_config { }
+  }
+
+  metadata_startup_script = "${file("./scripts/apt-get-ansible.sh")}"
+}
+
+resource "google_compute_instance" "webapp02" {
+
+  name = "app02"
+  zone = "europe-west1-d"
+  machine_type = "n1-standard-2"
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-1710"
+    }
+  }
+  network_interface {
+    network = "default"
+    access_config { }
+  }
+
+  metadata_startup_script = "${file("./scripts/apt-get-ansible.sh")}"
+}
+
+resource "google_compute_instance" "database" {
+
+  name = "db"
   zone = "europe-west1-d"
   machine_type = "n1-standard-2"
   boot_disk {
