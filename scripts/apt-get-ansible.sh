5fd504d5dc260f188d34430a920a3d4a3a83cc2d
Loïc <ldivad@isep.fr>
2018-03-25 01:05:33 +0200

    interpolate default variables

--- scripts/apt-get-ansible.sh
+++ scripts/apt-get-ansible.sh
@@ -7,4 +7,5 @@ sudo apt-get update -y
 sudo apt-get install ansible -y
 
 su - loicmdivad -s /bin/bash -c "ssh-keygen -t rsa -q -N '' -f ~/.ssh/id_rsa"
+su - loicmdivad -s /bin/bash -c "echo 'masteringansible' > /home/loicmdivad/.vault_pass.txt"
 su - loicmdivad -s /bin/bash -c "git clone https://github.com/DivLoic/gcp-ansible-playground.git"
